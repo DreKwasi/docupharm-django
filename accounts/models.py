4b303f28b2f03604b724e2336e828a270c5ad7e0
DreKwasi <andrewsboateng137@gmail.com>
2023-01-28 08:28:50 +0000

    Initial Setup

---
+++ accounts/models.py
@@ -0,0 +1,158 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+# Custom Account to define create user commands
+class MyAccountManager(BaseUserManager):
+    def create_user(
+        self, first_name, last_name, email, username, password=None
+    ):
+        """
+        Creates a new user and assign the expected variables to them
+        """
+        if not email:
+            raise ValueError("Users must have a valid email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        first_name,
+        last_name,
+        email,
+        username,
+        password=None,
+    ):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+# Custom User Model Inheriting from the AbstractBaseUser
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(
+        max_length=100, unique=True, verbose_name="email address"
+    )
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    # required
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "username"]
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does a User Have A specific Permission?"
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+
+class Location(models.Model):
+
+    city = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="City"
+    )
+    region = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="Region"
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Locations"
+
+
+
+class Profile(models.Model):
+
+    gender_type_choices = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+        ("Rather Not Say", "Rather Not Say"),
+    )
+    status_type_choices = (
+        ("Student Intern", "Student Intern"),
+        ("Intern Pharmacist/House Officer", "Intern Pharmacist/House Officer"),
+        ("Pharmacist (B.Pharm)", "Pharmacist (B.Pharm)"),
+        ("Pharmacist (Pharm.D)", "Pharmacist (Pharm.D)"),
+    )
+    account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, related_name="accounts_profile")
+    professional_status = models.CharField(
+        choices=status_type_choices, max_length=100, blank=True)
+    gender = models.CharField(
+        max_length=100, choices=gender_type_choices
+    )
+    registration_number = models.CharField(max_length=6, unique=True)
+
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{9,15}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True
+    ) 
+    # Validators should be a list
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Facility"
+        verbose_name_plural = "Facilities"
+
+    def __str__(self):
+        return self.facility_name
+
+
+class Employer(models.Model):
+    category_choices = (
+        ("Hospital", "Hospital"),
+        ("Community", "Community"),
+    )
+    account = models.ForeignKey(Account, related_name="accounts_employer", on_delete=models.SET_NULL) 
+    place_of_work = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, related_name="employer_locations")
+    category = models.CharField(choices=category_choices, max_length=50)
+    
+
