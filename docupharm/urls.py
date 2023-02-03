88d269e49ec924e675478ef905b1c82f0f835b0a
DreKwasi <andrewsboateng137@gmail.com>
2023-02-03 11:08:56 +0000

    Urls for media files

--- docupharm/urls.py
+++ docupharm/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("auth.api.urls")),
-    path("api/accounts", include("accounts.api.urls")),
-    path("api/interventions", include("interventions.api.urls"))
-    
+    path("api/accounts/", include("accounts.api.urls")),
+    path("api/interventions/", include("interventions.api.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
